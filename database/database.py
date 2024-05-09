32de2f2f7361ce60304bdc3ab483d7290b426f47
DRomanov <lukaviy2la@gmail.com>
2024-05-09 15:10:43 +0300

    add DB and CACH url in settings

--- database/database.py
+++ database/database.py
@@ -4,13 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from core.settings import settings
 
 
-DATABASE_URL = f"postgresql+asyncpg://postgres:{settings.DB_PASSWORD}@localhost:5432/{settings.DB_NAME}"
+DATABASE_URL = settings.ASYNC_DATABASE_URL
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-
-# async def get_db_session() -> AsyncSession:
-#     async with async_session() as session:
-#         yield session
