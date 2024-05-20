097b95b68495db26e77ade297b89808ad6459557
DRomanov <lukaviy2la@gmail.com>
2024-05-20 20:14:33 +0300

    refactoring/ change all on asyncio style

--- clients/google_client.py
+++ clients/google_client.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-import requests
+import httpx
 
 from schemas import GoogleUserData
 from core.settings import settings
@@ -8,18 +8,20 @@ from core.settings import settings
 
 @dataclass
 class GoogleClient():
-    settings = settings
+    async_client: httpx.AsyncClient
+    settings: settings
 
-    def get_user_info(self, code: str) -> dict:
-        access_token = self._get_user_access_token(code)
-        user_info = requests.get(
-            url="https://www.googleapis.com/oauth2/v1/userinfo",
-            headers={"Authorization": f"Bearer {access_token}"}
-        )
+    async def get_user_info(self, code: str) -> dict:
+        access_token = await self._get_user_access_token(code)
+        async with self.async_client as client:
+            user_info = await client.get(
+                url="https://www.googleapis.com/oauth2/v1/userinfo",
+                headers={"Authorization": f"Bearer {access_token}"}
+                )
         return GoogleUserData(**user_info.json(), access_token=access_token)
 
     
-    def _get_user_access_token(self, code: str):
+    async def _get_user_access_token(self, code: str):
         data = {
             "code": code,
             "client_id": self.settings.GOOGLE_CLIENT_ID,
@@ -27,11 +29,11 @@ class GoogleClient():
             "redirect_uri": self.settings.GOOGLE_REDIRECT_URI,
             "grant_type": "authorization_code",
         }
-        response = requests.post(
-            url=self.settings.GOOGLE_TOKEN_URL, data=data
-            )
+        async with self.async_client() as client:
+            response = await client.post(
+                url=self.settings.GOOGLE_TOKEN_URL, data=data
+                )
         access_token = response.json()["access_token"]
 
-        return access_token
-    
+        return access_token    
     
--- service/depends.py
+++ service/depends.py
@@ -1,22 +1,22 @@
 from fastapi import Depends, HTTPException, Request, security, Security, status
+import httpx
 
+from clients import GoogleClient, YandexClient
+from core.settings import settings
 from service.task_service import TaskService
 from service.category_service import CategoryService
 from service.user_service import UserService
 from service.auth_service import AuthService
 from utils.unitofwork import AbstractUnitOfWork, UnitOfWork
 from utils import UserNotFoundException, UserNotAwailable, TokenExpireError, TokenNotValidError
-from repository import TaskCache
-from cache import get_redis_connection
+
 
 
 async def get_task_service(uow: AbstractUnitOfWork = Depends(UnitOfWork)) -> TaskService:
     return TaskService(uow)
 
-
 async def get_category_service(uow: AbstractUnitOfWork = Depends(UnitOfWork)) -> CategoryService:
     return CategoryService(uow)
-
 
 async def get_user_service() -> UserService:
     return UserService()
@@ -38,7 +38,11 @@ async def get_request_user_id(
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=err.detail) 
     return user_id
 
+async def get_async_client() -> httpx.AsyncClient:
+    return httpx.AsyncClient
 
-# def get_task_cache_repo() -> TaskCache:
-#     redis_connection = get_redis_connection()
-#     return TaskCache(redis_connection)
+async def get_google_client(async_client: httpx.AsyncClient = Depends(get_async_client)):
+    return GoogleClient(async_client=async_client, settings=settings)
+
+async def get_yandex_client(async_client: httpx.AsyncClient = Depends(get_async_client)):
+    return YandexClient(async_client=async_client, settings=settings)
